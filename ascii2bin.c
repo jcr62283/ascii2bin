1dc580b9736ef6dd69c23209f0eb9005f635709d
jcr62283 <jericho.raz.584@my.csun.edu>
2021-03-02 21:29:46 -0800

    Filled in top of file and added comments

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,12 +1,12 @@
-/****************************************/
-/* Program Name: ascii2bin              */
-/* Author: Jericho Raz                  */
-/* Date: 3/2/21                         */
-/****************************************/
-/* Description: checksum algorithm in C */
-/* Validation Checks:                   */
-/* Enhancements: None                   */
-/****************************************/
+/*
+ * Program Name: ascii2bin
+ * Author: Jericho Raz
+ * Date: 3/2/21
+ * Description: Converts ASCII strings of binary numbers into binary
+ * Validation Checks:
+ *  - Input only consists of 1, 0, \n
+ * Enhancements: None
+ */
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -23,26 +23,24 @@ int main(int argc, char* argv[], char** envp)
 
     int retval = read(0, &ascii_value, 1);
 
-    while (retval == 1)
+    /* Keep reading bits until NL or EOF */
+    while (retval == 1 && ascii_value[0] != 10)
     {
-	byte readbyte = ascii_value[0];
+        byte readbyte = ascii_value[0];
 
-	if (!(readbyte == 10 || readbyte == 48 || readbyte == 49))
-	{
-	    fprintf(stderr, "Error in input. Digit %d is not a binary digit.\n", readbyte - offset);
-	    return 1;
-	}
+        /* Exit program if number entered is not in binary */
+        if (!(readbyte == 10 || readbyte == 48 || readbyte == 49))
+        {
+            fprintf(stderr, "Error in input. Digit %d is not a binary digit.\n", readbyte - offset);
+            return 1;
+        }
 
-        if (readbyte == 10)
-	{
-	    break;
-	}
-
+        /* Calculate new number based on new bit */
         int digit = readbyte - offset;
         number = (number << 1) + digit;
         retval = read(0, &ascii_value, 1);
     }
-        
+
 
     printf("%u\n", number);
     return 0;
